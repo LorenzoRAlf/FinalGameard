fddab559af693cd18d78b3fd26b618838d8db6bf
Lorenzo Ruiz <lruizalfieri@gmail.com>
2022-01-28 16:46:37 +0100

    first commit

---
+++ desktop/src/com/mygdx/juegardofinal/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.juegardofinal.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.juegardofinal.JuegardoFinal;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JuegardoFinal(), config);
+	}
+}
